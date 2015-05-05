fe5e78d084672ce7a67543da0cf87c0f9d9a2560
Allen Wu <allenwu1018@gmail.com>
2015-05-06 01:01:53 +0800

    add softlink for .vim & .vimrc when there's none

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 mkdir -p bundle
 git clone https://github.com/gmarik/Vundle.vim.git bundle/Vundle.vim
 
-# TODO: should detect if ~/.vim exists
 curr=`pwd`
-ln -s $curr ~/.vim
+[ ! -e "$HOME/.vim" ] && ln -s $curr $HOME/.vim
+[ ! -e "$HOME/.vimrc" ] && ln -s $curr/vimrc $HOME/.vimrc
